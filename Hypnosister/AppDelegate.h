9fe9a63817a57bece153012c67e5915a4489cb1d
Joshua Motley <joshmotley12@Joshua-Motleys-MacBook-Pro.local>
2015-07-28 20:27:02 -0400

    upload project

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Joshua Motley on 6/20/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/HypnosisView.h
@@ -0,0 +1,14 @@
+//
+//  UIView+HypnosisView.h
+//  Hypnosister
+//
+//  Created by Joshua Motley on 6/22/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisView : UIView
+@property (nonatomic) CGRect frame;
+
+@end
