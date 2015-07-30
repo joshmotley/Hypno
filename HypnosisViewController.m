6d71ef0335d83f9d9d9f7022e37d914a52a62e1a
Josh <Joshkmotley@gmail.com>
2015-07-30 15:02:10 -0400

    add labels to the screen

--- HypnosisViewController.m
+++ HypnosisViewController.m
@@ -9,6 +9,11 @@
 #import "HypnosisViewController.h"
 #import "HypnosisView.h"
 
+@interface HypnosisViewController ()
+<UITextFieldDelegate>
+
+@end
+
 @implementation HypnosisViewController : UIViewController
 
 -(void)loadView
@@ -53,7 +58,11 @@
                                    (scrollView.frame.size.height / 2)-(scrollView.frame.size.height/3));
     textField.borderStyle = UITextBorderStyleRoundedRect;
     textField.placeholder = @"Insert Text Here";
+    textField.returnKeyType = UIReturnKeyDone;
     [hypnosisView addSubview:textField];
+    
+    // Make HypnosisViewController the textField's delegate
+    textField.delegate = self;
     
     //Set the view of this view controller
     
@@ -67,5 +76,52 @@
         self.tabBarItem.image = i;}
     return self;
 }
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self drawHypnoticMessage:textField.text];
+    
+    textField.text = @" ";
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(void)drawHypnoticMessage:(NSString *)message
+{ for (int i=0; i<20; i++) {
+    UILabel *messageLabel = [[UILabel alloc] init];
+    
+    // Configure the label's color
+    
+    messageLabel.backgroundColor = [UIColor clearColor];
+    messageLabel.textColor = [UIColor whiteColor];
+    messageLabel.text = message;
+    
+    /* This method resizes the label, which will be relative
+    to the text that it is displaying */
+    
+    [messageLabel sizeToFit];
+    
+    // Get a random x value that fits within the hypnosis view's width
+    
+    int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+    
+    int x = arc4random() % width;
+    
+    // Get a random y value that fits within the hypnosis view's height
+    
+    int height = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+    int y = arc4random() % height;
+    
+    // Update the label's frame
+    
+    CGRect frame = messageLabel.frame;
+    frame.origin = CGPointMake(x, y);
+    messageLabel.frame = frame;
+    
+    // Add the label to the hierarchy
+    
+    [self.view addSubview:messageLabel]; }
+}
+
         
 @end
